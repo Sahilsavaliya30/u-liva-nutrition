5d6a511d1be5b495bc464b51e3869fbeaed35fac
Sahilsavaliya30 <73212057+Sahilsavaliya30@users.noreply.github.com>
2024-08-21 01:56:36 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -2,4 +2,16 @@ new WOW().init();
 var $ = jQuery.noConflict();
 jQuery(document).ready(function ($) {
 
+    var header_height = $(".header-main").outerHeight();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 350) {
+            $(".header-main").addClass("has-sticky");
+            $("body").addClass("sticky-header");
+            $("body").css("padding-top", header_height);
+        } else {
+            $(".header-main").removeClass("has-sticky");
+            $("body").removeClass("sticky-header");
+            $("body").css("padding-top", 0);
+        }
+    });
 });
