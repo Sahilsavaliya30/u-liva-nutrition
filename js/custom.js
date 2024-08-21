2f341065d793a999d199753e97afd2eabe520c98
Sahilsavaliya30 <73212057+Sahilsavaliya30@users.noreply.github.com>
2024-08-22 01:45:47 +0530

    update

--- js/custom.js
+++ js/custom.js
@@ -2,6 +2,18 @@ new WOW().init();
 var $ = jQuery.noConflict();
 jQuery(document).ready(function ($) {
 
+    $(".menu-icon").click(function () {
+        $(this).toggleClass("menu-close");
+        $(".navigation-bar").toggleClass("slide-menu");
+        $("body").addClass("body-fixed");
+    });
+
+    $(".sidebar-overlay, .close-menu").click(function () {
+        $(".menu-icon").removeClass("menu-close");
+        $(".navigation-bar").removeClass("slide-menu");
+        $("body").removeClass("body-fixed");
+    });
+
     var header_height = $(".header-main").outerHeight();
     $(window).scroll(function () {
         if ($(this).scrollTop() > 350) {
@@ -13,5 +25,34 @@ jQuery(document).ready(function ($) {
             $("body").removeClass("sticky-header");
             $("body").css("padding-top", 0);
         }
+    });
+
+    $(".latest-blog-wrapper").slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        speed: 500,
+        infinite: true,
+        pauseOnHover: false,
+        pauseOnFocus: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                },
+            },
+        ],
     });
 });
